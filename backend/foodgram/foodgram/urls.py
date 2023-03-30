5511b49c600b0c2c99259c97af1164beb7ede75c
aleksandr <aleksospishev@yandex.ru>
2023-03-30 20:24:37 +0300

    create app foodgram

---
+++ backend/foodgram/foodgram/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
+
+
+""" взяты из api_final!!!!
+необходимо доработать """
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('api.urls')),
+    path('redoc/',
+         TemplateView.as_view(template_name='redoc.html'),
+         name='redoc'),
+]
