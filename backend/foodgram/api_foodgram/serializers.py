b07c30ea9eddf8099ffb6ad9c80629eb6ac7b08f
aleksandr <aleksospishev@yandex.ru>
2023-06-02 12:31:17 +0300

    SIX review

--- backend/foodgram/api_foodgram/serializers.py
+++ backend/foodgram/api_foodgram/serializers.py
@@ -218,8 +218,8 @@ class SubscribeSerializer(serializers.ModelSerializer):
         )
 
     def get_is_subscribed(self, obj):
-        request = self.context.get('request').user
-        if not request or request.is_anonymous:
+        user = self.context.get('request').user
+        if not user or user.is_anonymous:
             return False
         return Subscribe.objects.filter(
             user=obj.user,
