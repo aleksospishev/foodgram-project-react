9a80b2d6a93af8f47b1b7a0ad5cdce3b20afb332
aleksandr <aleksospishev@yandex.ru>
2023-06-04 16:24:27 +0300

    SIX review

--- backend/foodgram/api_foodgram/serializers.py
+++ backend/foodgram/api_foodgram/serializers.py
@@ -238,9 +238,9 @@ class SubscribeSerializer(serializers.ModelSerializer):
         return obj.recipes.count()
 
     def create(self, validated_data):
-        # author = validated_data.pop('author')
+        author = self.context.get('author')
         user = self.context.get('request').user
-        return Subscribe.objects.create(author='author', user=user)
+        return Subscribe.objects.create(author=author, user=user)
 
     def validate(self, data):
         if self.context['request'].method != 'POST':
